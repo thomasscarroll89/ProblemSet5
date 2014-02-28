f9f5995a833a526026136cdc695894840164f1cc
thomasscarroll89 <thomasscarroll89>
2014-02-27 23:09:15 -0600

    Updated Problem 3
    Had a problem with the following line of code in problem 3:
    apply((voter.position - party.1)^2, 1, sum)
    
    I'm not sure this was working correctly. When I tried subtracting
    voter.position and party.1, it would subtract it in the reverse order
    that we wanted. (i.e. instead of subtracting the two values of party.1
    across the rows like we wanted, it was subtracting down the columns...so
    the first column of voter.positions was sometimes having the first value
    of party.1 being subtracted from it, and other times it was having the
    second value of party.1 being subtracted from it.)
    
    To be safe I decided to calculate the difference in positions as a
    matrix first, then go through and add them up. This may have been
    unnecessary, in which case I'm sorry for making the code more complex. I
    can try and describe in better detail what I think was wrong with the
    old code if you want, just let me know.

--- ProblemSet5.R
+++ ProblemSet5.R
@@ -3,8 +3,6 @@
 library(MASS)
 install.packages("pdist")
 library(pdist)
-# Simulation set up
-# Create a matrix of voters
 
 ####Problems 1-2####
 #The function "voters" generates an nx2 matrix of voter preferences, where each row corresponds to a different voter
@@ -44,27 +42,47 @@ voters <- function(n, option=1, sig1=runif(1), sig2=runif(1), Sigma=matrix(c(10,
   }
 }
 
-#changed the number of samples from "n*2" to "n" 
-#changed the way we sampled for option=5; the old way might have been mixing up results from different columns of the multivariate distribution
-
-
-
-# 3. Function such that voters affiliate with the closes of the two parties. 
-  party.1 <- rnorm(2)
-  party.2 <- rnorm(2)
-party.position <- rbind(party.1, party.2)
-party.position
-  voter.position <-  voters(n=100, option=1)  
-  distance.1 <- apply((voter.position - party.1)^2, 1, sum)
-  distance.1 <- sqrt(distance.1)
-  distance.2 <- apply((voter.position - party.2)^2, 1, sum)
-  distance.2 <- sqrt(distance.2)
+####Problem 3: Function such that voters affiliate with the closest of the two parties####
+#The affiliation function takes the same arguments as the voters function above; thus when running affiliation, you can 
+#decide which kind of distribution will be used to generate voter preferences, etc. 
+affiliation <- function(n, option=1, sig1=runif(1), sig2=runif(1), Sigma=matrix(c(10,3,3,2),2,2)){
+  voter.position <-  voters(n=n, option=option, sig1=sig1, sig2=sig2, Sigma=Sigma)  #run the voters function from Problems 1-2 above to generate some random initial voter preferences
+  party.1 <- rnorm(2) #the original positions taken by Party 1 on two dimensions
+  party.2 <- rnorm(2) #the original positions taken by Party 2 on two dimensions
+  
+  #Calculate a vector showing each individual's total distance from party 1; call it distance.1
+  distance.temp.1.1 <- voter.position[,1] - party.1[1] 
+  distance.temp.1.2 <- voter.position[,2] - party.1[2] 
+  distance.temp.1 <- cbind(distance.temp.1.1, distance.temp.1.2)^2 #matrix; first column is a^2, second column is b^2
+  distance.1 <- sqrt(apply(distance.temp.1, 1, sum)) #Pythagorean Theorem: c=sqrt(a^2 + b^2)
+  
+  #Next calculate a vector showing each individual's total distance from party 2; call it distance.2
+  distance.temp.2.1 <- voter.position[,1] - party.2[1]
+  distance.temp.2.2 <- voter.position[,2] - party.2[2]
+  distance.temp.2 <- cbind(distance.temp.2.1, distance.temp.2.2)^2 #matrix; first column is a^2, second column is b^2
+  distance.2 <- sqrt(apply(distance.temp.2, 1, sum)) #Pythagorean Theorem: c=sqrt(a^2 + b^2)
+  
   final.distance <- distance.1 - distance.2
   voter.position <- data.frame(voter.position)
   voter.position$final.distance <- final.distance
-  voter.position$affiliation <- ifelse(final.distance > 0, "2", "1")
- 
- # 4. 
+  voter.position$affiliation <- ifelse(final.distance > 0, "2", "1") 
+    #create affiliation variable. Coded as "2" if voter is closer to party 2, and "1" otherwise
+  return(voter.position)
+}
+
+first.draw <- affiliation(n=100)
+
+
+# the lines of code for problem 3 that were like: apply((voter.position - party.1)^2, 1, sum), I'm not sure they were 
+#working correctly. When I tried subtracting voter.position and party.1, it would subtract it in the reverse order that
+#we wanted. I'm not sure if it was also doing that outside the apply function, but just to be safe I decided to calculate 
+#the difference in positions as a matrix first, then go through and add them up. This may have been unnecessary, in which case I'm 
+#sorry for making the code more complex. I can try and describe in better detail what I think was wrong with the old code if you 
+#want, just let me know. 
+
+
+
+# 4. 
 colnames(party.position) <- c("issue1", "issue2")
 library(ggplot2)
 plot1 <- ggplot(voter.position, aes(x=X1, y=X2, colour=affiliation)) + geom_point() 
