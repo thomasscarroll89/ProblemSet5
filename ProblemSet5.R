e0650617269abde0b381ac41c99b4352b773f4e8
thomasscarroll89 <thomasscarroll89>
2014-02-27 21:38:30 -0600

    Changes to Problems 1-2
    I changed the number of samples from "n*2" to "n" for options 4 and 5,
    when we're sampling from multivariate normal distributions. The reason
    for this is because multivariate normal random samples are vectors equal
    to the number of parameters (in this case 2), so by multiplying it by 2
    we were getting twice as many observations as we wanted.
    I also changed the way we sampled for option=5; the old way might have
    been mixing up results from different columns of the multivariate
    distribution.
    Finally, I changed the output of the function from have five different
    potential outputs (e.g. voter1, voter2, etc.) to all be labeled "voter";
    I don't think this should cause any problems since we're only generating
    one matrix at a time.

--- ProblemSet5.R
+++ ProblemSet5.R
@@ -6,37 +6,50 @@ library(pdist)
 # Simulation set up
 # Create a matrix of voters
 
-voters <- function(n, sig1=runif(1), sig2=runif(1), Sigma=matrix(c(10,3,3,2),2,2), option){
-  # 2. Create voter preferences
-  # Set the option to get different voter preferences matrix
+####Problems 1-2####
+#The function "voters" generates an nx2 matrix of voter preferences, where each row corresponds to a different voter
+#and each column corresponds to a specific policy dimension. The "option" argument determines how the preferences are 
+#to be drawn (i.e. whether they should be drawn from a normal distribution, uniform distribution, etc.). For this problem,
+#"option" can be set to any integer from 1 to 5. If option=1, then the voter preferences are drawn from a standard normal 
+#distribution. If option=2, then the preferences are drawn from 2 separate normal distributions with  standard deviations 
+#equal to sig1 and sig2, respectively. If option=3, preferences are drawn from a uniform distribution. If option=4, preferences
+#are drawn from a multivariate normal distribution with variance-covariance matrix equal to argument "Sigma". If option=5,
+#then 3 separate multivariate normal distributions with random parameters are used to generate preferences, and from these 3 
+#distributions a random sample of size n is selected and used as the matrix of voter preferences. 
+voters <- function(n, option=1, sig1=runif(1), sig2=runif(1), Sigma=matrix(c(10,3,3,2),2,2)){
   if (option==1){
-    voter.1 <- matrix(rnorm(n*2), nrow=n, ncol=2)
-    return(voter.1)
+    voter <- matrix(rnorm(n*2), nrow=n, ncol=2)
+    return(voter)
   }
   if (option==2){
-    voter.2 <- matrix(c(rnorm(n, mean=0, sd=sig1), rnorm(n, mean=0, sd=sig2)),
+    voter <- matrix(c(rnorm(n, mean=0, sd=sig1), rnorm(n, mean=0, sd=sig2)),
                       nrow=n, ncol=2)
-    return(voter.2)
+    return(voter)
   }
   if (option==3){
-    voter.3 <- matrix(runif(n*2), nrow=n, ncol=2)
-    return(voter.3)
+    voter <- matrix(runif(n*2), nrow=n, ncol=2)
+    return(voter)
   }
   if (option==4){
-    voter.4 <- matrix(mvrnorm(n*2, mu=c(0,0), Sigma=Sigma),nrow=n, ncol=2)
-    return(voter.4)
+    voter <- matrix(mvrnorm(n, mu=c(0,0), Sigma=Sigma),nrow=n, ncol=2)
+    return(voter)
   }
   if (option==5){
-    mvr.1 <- mvrnorm(n*2, mu=c(0,0), Sigma=Sigma)
-    mvr.2 <- mvrnorm(n*2, mu=runif(2), Sigma=Sigma+runif(1))
-    mvr.3 <- mvrnorm(n*2, mu=runif(2), Sigma=Sigma+runif(1))
+    mvr.1 <- mvrnorm(n, mu=c(0,0), Sigma=Sigma)
+    mvr.2 <- mvrnorm(n, mu=runif(2), Sigma=Sigma+runif(1))
+    mvr.3 <- mvrnorm(n, mu=runif(2), Sigma=Sigma+runif(1))
     total <- rbind(mvr.1, mvr.2, mvr.3)
-    voter.5 <- matrix(sample(total, size=n*2), nrow=n, ncol=2)
-    return(voter.5)
+    voter <- total[sample(1:(3*100), size=100),]
+    return(voter)
   }
 }
-  
-  # 3. Function such that voters affiliate with the closes of the two parties. 
+
+#changed the number of samples from "n*2" to "n" 
+#changed the way we sampled for option=5; the old way might have been mixing up results from different columns of the multivariate distribution
+
+
+
+# 3. Function such that voters affiliate with the closes of the two parties. 
   party.1 <- rnorm(2)
   party.2 <- rnorm(2)
 party.position <- rbind(party.1, party.2)
